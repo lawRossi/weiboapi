d9810f79f9a0508610c6258c38dfbf863b561e83
Rossi <1142595193@qq.com>
2016-02-03 21:04:44 +0800

    reorganize code directory

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from os.path import dirname, join
 from setuptools import setup, find_packages  
   
 
-with open(join(dirname(__file__), 'VERSION'), 'r') as f:
+with open(join(dirname(__file__), 'weiboapi/VERSION'), 'r') as f:
     version = f.read().strip()
 
 
@@ -16,4 +16,9 @@ setup(
     description="Sina weibo api.",
     author="Rossi",
     packages=find_packages(exclude=("test", "test.*")),
+    install_requires=[
+        "lxml",
+        "rsa",
+        "BeautifulSoup4"
+    ]
 )
